975dfb68b075a92050b95070a6e22339242f1620
Kevin Pratt <prattkev@gmail.com>
2022-05-17 21:30:46 -0400

    Add project files.

---
+++ TestThing/Controllers/BooksController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestThing.Models;
+
+namespace TestThing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        public List<Book> books = new List<Book>()
+        {
+            new Book { Id = 1, Title = "title 1", Author = "Pratt, Kevin", PublicationYear = 2001, IsAvailable = true, CallNumber = "1a"},
+            new Book { Id = 2, Title = "title 2", Author = "Pratt, Kevin", PublicationYear = 2001, IsAvailable = true, CallNumber = "1s"},
+            new Book { Id = 3, Title = "title 3", Author = "Pratt, Kevin", PublicationYear = 2001, IsAvailable = true, CallNumber = "1d"},
+            new Book { Id = 4, Title = "title 4", Author = "Pratt, Kevin", PublicationYear = 2001, IsAvailable = true, CallNumber = "1f"},
+            new Book { Id = 5, Title = "title 5", Author = "Pratt, Kevin", PublicationYear = 2001, IsAvailable = true, CallNumber = "1b"}
+        };
+
+        [HttpGet]
+
+        public ActionResult<IEnumerable<Book>> GetAllBooks()
+        {
+            return books;
+        }
+
+        [HttpGet("{id}")]
+
+        public ActionResult<Book> GetBook(int id)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return book;
+        }
+
+    }
+}
---
+++ TestThing/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestThing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value 1", "value 2", "value 3", "value 4" };
+        }
+
+        [HttpGet("{id}")]
+
+        public string Get(int id)
+        {
+            return "The Value is " + id;
+        }
+
+    }
+}
